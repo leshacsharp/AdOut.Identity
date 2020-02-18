f85f35062a0b19e581c0f20f793ce99b539fe542
Starchenko Aleksey <A.Starchanka@sam-solutions.com>
2020-02-18 16:26:15 +0300

    Added main entities and database context

---
+++ src/AdOut.Identity.DataProvider/Context/IdentityContext.cs
@@ -0,0 +1,17 @@
+﻿using AdOut.Identity.Model.Database;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdOut.Identity.DataProvider.Context
+{
+    public class IdentityContext : IdentityDbContext<User>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> dbContextOptions)
+            : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Permission> Permissions { get; set; }
+    }
+}
---
+++ src/AdOut.Identity.Model/Database/Permission.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AdOut.Identity.Model.Database
+{
+    [Table("Permissions")]
+    public class Permission
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(maximumLength:15, MinimumLength=3)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Role> Roles { get; set; }
+    }
+}
