b7aeeb2d4e6df8b588e202cf1e2de06b1d862433
Starchenko Aleksey <A.Starchanka@sam-solutions.com>
2020-02-18 17:28:15 +0300

    Added abstraction for repositories

--- src/AdOut.Identity.DataProvider/Context/IdentityContext.cs
+++ src/AdOut.Identity.DataProvider/Context/IdentityContext.cs
@@ -1,15 +1,16 @@
 ﻿using AdOut.Identity.Model.Database;
+using AdOut.Identity.Model.Interfaces.Context;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AdOut.Identity.DataProvider.Context
 {
-    public class IdentityContext : IdentityDbContext<User>
+    public class IdentityContext : IdentityDbContext<User, Role, string>, IDatabaseContext
     {
         public IdentityContext(DbContextOptions<IdentityContext> dbContextOptions)
             : base(dbContextOptions)
         {
-
+            
         }
 
         public DbSet<Permission> Permissions { get; set; }
