8cf0ce756b43ba33230a27f5cf0432611831f8e3
Starchenko Aleksey <A.Starchanka@sam-solutions.com>
2020-02-20 16:11:01 +0300

    Integrated Swagger

--- src/AdOut.Identity.WebApi/Startup.cs
+++ src/AdOut.Identity.WebApi/Startup.cs
@@ -2,13 +2,13 @@ using AdOut.Identity.Core.DI;
 using AdOut.Identity.DataProvider.DI;
 using AdOut.Identity.DataProvider.Context;
 using AdOut.Identity.Model.Database;
-using AdOut.Identity.Model.Interfaces.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace AdOut.Identity.WebApi
 {
@@ -23,24 +23,37 @@ namespace AdOut.Identity.WebApi
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddControllers();
+            
             //todo: make Different connections for dev and prod configurations
-            services.AddDbContext<IDatabaseContext, IdentityContext>(options => 
+            services.AddDbContext<IdentityContext>(options => 
                      options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
 
-            services.AddIdentity<User, Role>()
-                    .AddEntityFrameworkStores<IdentityContext>();
+            services.AddIdentity<User, Role>(setup =>
+            {
+                setup.Password.RequireUppercase = false;
+                setup.Password.RequireDigit = false;
+                setup.Password.RequireNonAlphanumeric = false;
+            })
+            .AddEntityFrameworkStores<IdentityContext>();
+           
 
             services.AddDataProviderModule();
             services.AddCoreModule();
 
-            services.AddControllers();
-
-            var identityServerBuilder = services.AddIdentityServer()
+            var identityServerBuilder = services.AddIdentityServer(options => options.UserInteraction.LoginUrl = "/auth/login")
                 .AddInMemoryIdentityResources(IdentityServerConfig.Ids)
                 .AddInMemoryApiResources(IdentityServerConfig.Apis)
-                .AddInMemoryClients(IdentityServerConfig.Clients);
+                .AddInMemoryClients(IdentityServerConfig.Clients)
+                .AddAspNetIdentity<User>();
 
             identityServerBuilder.AddDeveloperSigningCredential();
+
+            services.AddSwaggerGen(setup =>
+            {
+                setup.SwaggerDoc("v1", new OpenApiInfo { Title = "AdOut.Identity API", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -56,9 +69,18 @@ namespace AdOut.Identity.WebApi
             app.UseIdentityServer();
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
+            app.UseSwagger();
+            app.UseSwaggerUI(setup =>
             {
-                endpoints.MapControllers();
+                setup.SwaggerEndpoint("/swagger/v1/swagger.json", "AdOut.Identity API V1");
+              //  setup.RoutePrefix = string.Empty;
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
