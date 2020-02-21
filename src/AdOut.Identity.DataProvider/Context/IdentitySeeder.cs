b35f2d4284ab33febf8b671bfbe28f342393313f
Starchenko Aleksey <star4enko.aleksey2015@yandex.ru>
2020-02-21 20:18:49 +0300

    Added default permissions in Database Seeder

--- src/AdOut.Identity.DataProvider/Context/IdentitySeeder.cs
+++ src/AdOut.Identity.DataProvider/Context/IdentitySeeder.cs
@@ -10,6 +10,13 @@ namespace AdOut.Identity.DataProvider.Context
     {
         public void Seed(ModelBuilder builder)
         {
+            var testPermission = new Permission()
+            {
+                Id = 1,
+                Name = "test permission"
+            };
+            builder.Entity<Permission>().HasData(testPermission);
+
             var adminRole = new Role()
             {
                 Id = "b1419175-af91-4dff-ba78-13201d7538f4",
@@ -17,6 +24,13 @@ namespace AdOut.Identity.DataProvider.Context
                 NormalizedName = "admin".ToUpper()
             };
             builder.Entity<Role>().HasData(adminRole);
+
+            var adminRolePermissions = new RolePermission()
+            {
+                RoleId = adminRole.Id,
+                PermissionId = testPermission.Id
+            };
+            builder.Entity<RolePermission>().HasData(adminRolePermissions);
 
             var hasher = new PasswordHasher<User>();
             var adminUser = new User()
@@ -33,11 +47,12 @@ namespace AdOut.Identity.DataProvider.Context
             };
             builder.Entity<User>().HasData(adminUser);
 
-            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>()
+            var adminUserRole = new IdentityUserRole<string>()
             {
                 UserId = adminUser.Id,
                 RoleId = adminRole.Id
-            });
+            };
+            builder.Entity<IdentityUserRole<string>>().HasData(adminUserRole);
         }
     }
 }
